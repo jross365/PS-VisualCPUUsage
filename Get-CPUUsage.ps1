de662501f94be791c6bc738e97bc77231eb88597
jross365 <jross365github@gmail.com>
2023-02-12 09:47:29 -0500

    Beginning restructure

--- Get-CPUUsage.ps1
+++ Get-CPUUsage.ps1
@@ -23,102 +23,112 @@ Function Get-CPUUsage {
     [CmdletBinding()] 
     param( 
         [Parameter(Mandatory=$False)][Alias('Poll')][int]$PollingIntervalMS,
-        [Parameter(ParameterSetName='Logging',Mandatory=$False)][string]$LogFile,
-        [Parameter(ParameterSetName='Logging',Mandatory=$False)][switch]$IncludeAverage,
-        [Parameter(ParameterSetName='Logging',Mandatory=$False)][switch]$UnixTime
+        [Parameter(ParameterSetName='Logging')][string]$LogFile,
+        [Parameter(ParameterSetName='Logging')][switch]$IncludeAverage,
+        [Parameter(ParameterSetName='Logging')][switch]$UnixTime,
+        [Parameter(Mandatory=$False)][switch]$Quiet
         
         )
 
 begin {        
 
-If ($null -eq $Global:CPUParams){
+switch ($Quiet.IsPresent){
 
-    $Global:CPUParams = @{}
+$true {}
 
-    $Global:CPUParams.Add("Cores",(Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors)
+$false {
 
-#region 0. Identify preferred X/Y Grid mapping
+    If ($null -eq $Global:CPUParams){
 
-    $SqRt = [system.math]::Sqrt($Global:CPUCores)
-
-    If ($SqRt.GetType().Name -match "Int"){
+        $Global:CPUParams = @{}
     
-            $Global:CPUParams.Add("X",$SqRt)
-            $Global:CPUParams.Add("Y",$SqRt)
-
-            } #Close if square root
-    Else {
-
-    $CoordTest = New-Object System.Collections.ArrayList
-    $UpperLimit = ([system.math]::Ceiling([system.math]::Sqrt($Global:CPUParams.Cores))) * 2
-
-    (2..$UpperLimit).ForEach({$CoordTest.Add([pscustomobject]@{"X"=$_; "Y"=($Global:CPUParams.Cores / $_); "Diff"=($null)}) | Out-Null})
+        $Global:CPUParams.Add("Cores",(Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors)
     
-    $CoordTest.ForEach({$_.Diff = [System.Math]::abs(($_.X - $_.Y))})
-    $CoordTest = $CoordTest.Where({[string]($_.Diff) -notmatch '\.'}) | Sort-Object Diff
-
-    $ChosenCoords = $CoordTest.Where({$_.Diff -eq ($CoordTest[0].Diff) -and ($_.X -ge $_.Y)})
-
-    Try {$Global:CPUParams.Add("X",($ChosenCoords.X))}
-    Catch {$Global:CPUParams.'X' = $ChosenCoords.X}
+    #region 0. Identify preferred X/Y Grid mapping
     
-    Try {$Global:CPUParams.Add("Y",($ChosenCoords.Y))}
-    Catch {$Global:CPUParams.'Y' = $ChosenCoords.Y}
+        $SqRt = [system.math]::Sqrt($Global:CPUCores)
+    
+        If ($SqRt.GetType().Name -match "Int"){
+        
+                $Global:CPUParams.Add("X",$SqRt)
+                $Global:CPUParams.Add("Y",$SqRt)
+    
+                } #Close if square root
+        Else {
+    
+        $CoordTest = New-Object System.Collections.ArrayList
+        $UpperLimit = ([system.math]::Ceiling([system.math]::Sqrt($Global:CPUParams.Cores))) * 2
+    
+        (2..$UpperLimit).ForEach({$CoordTest.Add([pscustomobject]@{"X"=$_; "Y"=($Global:CPUParams.Cores / $_); "Diff"=($null)}) | Out-Null})
+        
+        $CoordTest.ForEach({$_.Diff = [System.Math]::abs(($_.X - $_.Y))})
+        $CoordTest = $CoordTest.Where({[string]($_.Diff) -notmatch '\.'}) | Sort-Object Diff
+    
+        $ChosenCoords = $CoordTest.Where({$_.Diff -eq ($CoordTest[0].Diff) -and ($_.X -ge $_.Y)})
+    
+        Try {$Global:CPUParams.Add("X",($ChosenCoords.X))}
+        Catch {$Global:CPUParams.'X' = $ChosenCoords.X}
+        
+        Try {$Global:CPUParams.Add("Y",($ChosenCoords.Y))}
+        Catch {$Global:CPUParams.'Y' = $ChosenCoords.Y}
+    
+        } #Close Else
+    
+    } #Close If $null eq $Global:CPUParams
+    
+    #endregion 0.
+    
+    #region 1. Build output template
+    $ExecutionTemplate = New-Object System.Collections.ArrayList
+    
+    $BaseString = 'Write-Host "[CC]" -ForegroundColor FG -BackgroundColor BG -NoNewLine; '
+    
+    $CoreCounter = 0
+    
+    $CoreRowHash = @{}
+    
+    (0..($Global:CPUParams.Y - 1)).ForEach({
+    
+    $Row = $_
+    
+    $LineString = ""
+    
+        (0..($Global:CPUParams.X - 1)).ForEach({
+    
+        $Column = $_
+    
+        $ReplString = $BaseString -replace 'CC',"CC$($CoreCounter)" -replace 'FG',"FG$($CoreCounter)" -replace 'BG',"BG$($CoreCounter)"
+    
+        If ($Column -eq ($Global:CPUParams.X - 1)){$ReplString = $ReplString -replace '-NoNewLine',''}
+    
+        $LineString += $ReplString
+    
+        $CoreRowHash.Add($CoreCounter,$Row)
+    
+        $CoreCounter++
+    
+        }) #Close X
+    
+    $ExecutionTemplate.Add($LineString) | Out-Null
+    
+    }) #Close Y
+    
+    #endregion 1.
+    
+    #region 2. Build %/Color Hash Table
+    $ColorHash = @{}
+    
+    (0..2).ForEach({$ColorHash.Add($_,[pscustomobject]@{"FG"="DarkCyan";"BG"="Cyan"})})
+    (3..49).ForEach({$ColorHash.Add($_,[pscustomobject]@{"FG"="DarkGreen";"BG"="Green"})})
+    (50..79).ForEach({$ColorHash.Add($_,[pscustomobject]@{"FG"="DarkYellow";"BG"="Yellow"})})
+    (80..99).ForEach({$ColorHash.Add($_,[pscustomobject]@{"FG"="DarkRed";"BG"="Red"})})
+    $ColorHash.Add(100,[pscustomobject]@{"FG"="White";"BG"="Red"})
+    
+    #endregion 2.
+    }
+}
 
-    } #Close Else
-
-} #Close If $null eq $Global:CPUParams
-
-#endregion 0.
-
-#region 1. Build output template
-$ExecutionTemplate = New-Object System.Collections.ArrayList
-
-$BaseString = 'Write-Host "[CC]" -ForegroundColor FG -BackgroundColor BG -NoNewLine; '
-
-$CoreCounter = 0
-
-$CoreRowHash = @{}
-
-(0..($Global:CPUParams.Y - 1)).ForEach({
-
-$Row = $_
-
-$LineString = ""
-
-    (0..($Global:CPUParams.X - 1)).ForEach({
-
-    $Column = $_
-
-    $ReplString = $BaseString -replace 'CC',"CC$($CoreCounter)" -replace 'FG',"FG$($CoreCounter)" -replace 'BG',"BG$($CoreCounter)"
-
-    If ($Column -eq ($Global:CPUParams.X - 1)){$ReplString = $ReplString -replace '-NoNewLine',''}
-
-    $LineString += $ReplString
-
-    $CoreRowHash.Add($CoreCounter,$Row)
-
-    $CoreCounter++
-
-    }) #Close X
-
-$ExecutionTemplate.Add($LineString) | Out-Null
-
-}) #Close Y
-
-#endregion 1.
-
-#region 2. Build %/Color Hash Table
-$ColorHash = @{}
-
-(0..2).ForEach({$ColorHash.Add($_,[pscustomobject]@{"FG"="DarkCyan";"BG"="Cyan"})})
-(3..49).ForEach({$ColorHash.Add($_,[pscustomobject]@{"FG"="DarkGreen";"BG"="Green"})})
-(50..79).ForEach({$ColorHash.Add($_,[pscustomobject]@{"FG"="DarkYellow";"BG"="Yellow"})})
-(80..99).ForEach({$ColorHash.Add($_,[pscustomobject]@{"FG"="DarkRed";"BG"="Red"})})
-$ColorHash.Add(100,[pscustomobject]@{"FG"="White";"BG"="Red"})
-
-#endregion 2.
-} #end Begin
+} #End Begin
 
 process {
 #region 3. Put it all together
